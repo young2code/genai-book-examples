83e268a78322d7c00841b215013de47d33410722
youngwook.yang <youngwook.yang@gmail.com>
2024-04-28 18:46:57 -0700

    Add CGAN example

--- CH04-CelebA-CGAN/main.py
+++ CH04-CelebA-CGAN/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from tensorflow.keras import (
     layers,
@@ -12,20 +13,31 @@ from tensorflow.keras import (
 # Parameters
 IMAGE_SIZE = 64
 CHANNELS = 3
+CLASSES = 2
+#BATCH_SIZE = 128
+#Z_DIM = 32
 BATCH_SIZE = 512
 Z_DIM = 128
-LEARNING_RATE = 0.0002
+LEARNING_RATE = 0.00005
 ADAM_BETA_1 = 0.5
 ADAM_BETA_2 = 0.999
-EPOCHS = 200
+EPOCHS = 20
 CRITIC_STEPS = 3
 GP_WEIGHT = 10.0
 LOAD_MODEL = False
+LABEL = "Blond_Hair"
 
-# Load the data
+# Load the label dataset
+attributes = pd.read_csv("./data/list_attr_celeba.csv")
+print(attributes.columns)
+attributes.head()
+labels = attributes[LABEL].tolist()
+int_labels = [x if x == 1 else 0 for x in labels]
+
+# Load the image data
 train_data = utils.image_dataset_from_directory(
-    "./data/img_align_celeba/img_align_celeba",
-    labels=None,
+    "./data/img_align_celeba",
+    labels=int_labels,
     color_mode="rgb",
     image_size=(IMAGE_SIZE, IMAGE_SIZE),
     batch_size=BATCH_SIZE,
@@ -41,44 +53,49 @@ def preprocess(img):
     return img
 
 
-train = train_data.map(lambda x: preprocess(x))
+train = train_data.map(lambda x, y: (preprocess(x), tf.one_hot(y, depth=CLASSES)))
 
-
-# Build WGAN-GP
+# Build CGAN
 
 # Critic
 critic_input = layers.Input(shape=(IMAGE_SIZE, IMAGE_SIZE, CHANNELS))
-x = layers.Conv2D(64, kernel_size=4, strides=2, padding="same")(critic_input)
+label_input = layers.Input(shape=(IMAGE_SIZE, IMAGE_SIZE, CLASSES))
+x = layers.Concatenate(axis=-1)([critic_input, label_input])
+
+x = layers.Conv2D(64, kernel_size=4, strides=2, padding="same")(x)
 x = layers.LeakyReLU(0.2)(x)
 
 x = layers.Conv2D(128, kernel_size=4, strides=2, padding="same")(x)
 x = layers.LeakyReLU(0.2)(x)
 x = layers.Dropout(0.3)(x)
 
-x = layers.Conv2D(256, kernel_size=4, strides=2, padding="same")(x)
+x = layers.Conv2D(128, kernel_size=4, strides=2, padding="same")(x)
 x = layers.LeakyReLU(0.2)(x)
 x = layers.Dropout(0.3)(x)
 
-x = layers.Conv2D(512, kernel_size=4, strides=2, padding="same")(x)
+x = layers.Conv2D(128, kernel_size=4, strides=2, padding="same")(x)
 x = layers.LeakyReLU(0.2)(x)
 x = layers.Dropout(0.3)(x)
 
 x = layers.Conv2D(1, kernel_size=4, strides=1, padding="valid")(x)
 critic_output = layers.Flatten()(x)
 
-critic = models.Model(critic_input, critic_output)
+critic = models.Model([critic_input, label_input], critic_output)
 critic.summary()
 
 # Generator
 
 generator_input = layers.Input(shape=(Z_DIM,))
-x = layers.Reshape((1, 1, Z_DIM))(generator_input)
+label_input = layers.Input(shape=(CLASSES,))
+x = layers.Concatenate(axis=-1)([generator_input, label_input])
 
-x = layers.Conv2DTranspose(512, kernel_size=4, strides=1, padding="valid", use_bias=False)(x)
+x = layers.Reshape((1, 1, Z_DIM + CLASSES))(x)
+
+x = layers.Conv2DTranspose(128, kernel_size=4, strides=1, padding="valid", use_bias=False)(x)
 x = layers.BatchNormalization(momentum=0.9)(x)
 x = layers.LeakyReLU(0.2)(x)
 
-x = layers.Conv2DTranspose(256, kernel_size=4, strides=2, padding="same", use_bias=False)(x)
+x = layers.Conv2DTranspose(128, kernel_size=4, strides=2, padding="same", use_bias=False)(x)
 x = layers.BatchNormalization(momentum=0.9)(x)
 x = layers.LeakyReLU(0.2)(x)
 
@@ -92,12 +109,12 @@ x = layers.LeakyReLU(0.2)(x)
 
 generator_output = layers.Conv2DTranspose(CHANNELS, kernel_size=4, strides=2, padding="same", activation="tanh")(x)
 
-generator = models.Model(generator_input, generator_output)
+generator = models.Model([generator_input, label_input], generator_output)
 generator.summary()
 
-class WGANGP(models.Model):
+class ConditionalWGANGP(models.Model):
     def __init__(self, critic, generator, latent_dim, critic_steps, gp_weight):
-        super(WGANGP, self).__init__()
+        super(ConditionalWGANGP, self).__init__()
         self.critic = critic
         self.generator = generator
         self.latent_dim = latent_dim
@@ -105,7 +122,7 @@ class WGANGP(models.Model):
         self.gp_weight = gp_weight
 
     def compile(self, c_optimizer, g_optimizer):
-        super(WGANGP, self).compile()
+        super(ConditionalWGANGP, self).compile(run_eagerly=True)
         self.c_optimizer = c_optimizer
         self.g_optimizer = g_optimizer
         self.c_wass_loss_metric = metrics.Mean(name="c_wass_loss")
@@ -122,33 +139,38 @@ class WGANGP(models.Model):
             self.g_loss_metric,
         ]
 
-    def gradient_penalty(self, batch_size, real_images, fake_images):
+    def gradient_penalty(self, batch_size, real_images, fake_images, image_one_hot_labels):
         alpha = tf.random.normal([batch_size, 1, 1, 1], 0.0, 1.0)
         diff = fake_images - real_images
         interpolated = real_images + alpha * diff
 
         with tf.GradientTape() as gp_tape:
             gp_tape.watch(interpolated)
-            pred = self.critic(interpolated, training=True)
+            pred = self.critic([interpolated, image_one_hot_labels], training=True)
 
         grads = gp_tape.gradient(pred, [interpolated])[0]
         norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=[1,2,3]))
         gp = tf.reduce_mean((norm - 1.0) ** 2)
         return gp
 
-    def train_step(self, real_images):
+    def train_step(self, data):
+        real_images, one_hot_labels = data
+        image_one_hot_labels = one_hot_labels[:, None, None, :]
+        image_one_hot_labels = tf.repeat(image_one_hot_labels, repeats=IMAGE_SIZE, axis=1)
+        image_one_hot_labels = tf.repeat(image_one_hot_labels, repeats=IMAGE_SIZE, axis=2)
+
         batch_size = tf.shape(real_images)[0]
 
         for i in range(self.critic_steps):
             random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
 
             with tf.GradientTape() as tape:
-                fake_images = self.generator(random_latent_vectors, training=True)
-                fake_predictions = self.critic(fake_images, training=True)
-                real_predictions = self.critic(real_images, training=True)
+                fake_images = self.generator([random_latent_vectors, one_hot_labels], training=True)
+                fake_predictions = self.critic([fake_images, image_one_hot_labels], training=True)
+                real_predictions = self.critic([real_images, image_one_hot_labels], training=True)
 
                 c_wass_loss = tf.reduce_mean(fake_predictions) - tf.reduce_mean(real_predictions)
-                c_gp = self.gradient_penalty(batch_size, real_images, fake_images)
+                c_gp = self.gradient_penalty(batch_size, real_images, fake_images, image_one_hot_labels)
                 c_loss = c_wass_loss + c_gp * self.gp_weight
 
             c_gradient = tape.gradient(c_loss, self.critic.trainable_variables)
@@ -156,8 +178,8 @@ class WGANGP(models.Model):
 
         random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
         with tf.GradientTape() as tape:
-            fake_images = self.generator(random_latent_vectors, training=True)
-            fake_predictions = self.critic(fake_images, training=True)
+            fake_images = self.generator([random_latent_vectors, one_hot_labels], training=True)
+            fake_predictions = self.critic([fake_images, image_one_hot_labels], training=True)
             g_loss = -tf.reduce_mean(fake_predictions)
 
         gen_gradient = tape.gradient(g_loss, self.generator.trainable_variables)
@@ -172,7 +194,7 @@ class WGANGP(models.Model):
 
 
 # Create WGAN-GP
-wgangp = WGANGP(
+cgan = ConditionalWGANGP(
     critic=critic,
     generator=generator,
     latent_dim=Z_DIM,
@@ -188,17 +210,28 @@ class ImageGenerator(callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs=None):
         random_latent_vectors = tf.random.normal(shape=(self.num_img, self.latent_dim))
-        generated_images = self.model.generator(random_latent_vectors)
+
+        # 0 label
+        zero_label = np.repeat([[1, 0]], self.num_img, axis=0)
+        generated_images = self.model.generator([random_latent_vectors, zero_label])
         generated_images = generated_images * 127.5 + 127.5
         generated_images = generated_images.numpy()
         for i in range(self.num_img):
             img = utils.array_to_img(generated_images[i])
-            img.save("./output/generated_img_%03d_%d.png" % (epoch, i))
+            img.save("./output/label_0/generated_img_%03d_%d.png" % (epoch, i))
 
+        # 1 label
+        one_label = np.repeat([[0, 1]], self.num_img, axis=0)
+        generated_images = self.model.generator([random_latent_vectors, one_label])
+        generated_images = generated_images * 127.5 + 127.5
+        generated_images = generated_images.numpy()
+        for i in range(self.num_img):
+            img = utils.array_to_img(generated_images[i])
+            img.save("./output/label_1/generated_img_%03d_%d.png" % (epoch, i))
 
 def fit_gan():
     # Compile the GAN
-    wgangp.compile(
+    cgan.compile(
         c_optimizer=optimizers.Adam(learning_rate=LEARNING_RATE, beta_1=ADAM_BETA_1, beta_2=ADAM_BETA_2),
         g_optimizer=optimizers.Adam(learning_rate=LEARNING_RATE, beta_1=ADAM_BETA_1, beta_2=ADAM_BETA_2),
     )
@@ -214,10 +247,11 @@ def fit_gan():
     tensorboard_callback = callbacks.TensorBoard(log_dir="./logs")
 
     # Fit
-    wgangp.fit(
+    cgan.fit(
         train,
-        epochs=EPOCHS,
-        steps_per_epoch=2,
+        epochs=20,
+        #epochs=EPOCHS * 100,
+        #steps_per_epoch=1,
         callbacks=[
             model_checkpoint_callback,
             tensorboard_callback,
@@ -231,6 +265,6 @@ def fit_gan():
 
 
 if LOAD_MODEL:
-    wgangp.load_weights("./checkpoint/checkpoint.ckpt")
+    cgan.load_weights("./checkpoint/checkpoint.ckpt")
 else:
     fit_gan()
